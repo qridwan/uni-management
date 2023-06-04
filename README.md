b70f6b152ac8a3ee6fb9a4ecef92141f0b1b2eb0
Ridwan <ridwanul.alam96@gmail.com>
2023-06-04 13:11:34 +0600

    done

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # uni-management
-# uni-management
+
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string);
+    await mongoose.connect(config.database_url as string)
 
-    console.log(`🛢   Database is connected successfully`);
+    console.log('🛢   Database is connected successfully')
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`);
-    });
+      console.log(`Application listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log("failed to connect", error);
+    console.log('failed to connect', error)
   }
 }
 
-bootstrap();
+bootstrap()
