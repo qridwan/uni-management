2cc8bd4df6b477c9f29a975319942daadb7f439e
Ridwan <ridwanul.alam96@gmail.com>
2023-06-09 02:40:19 +0600

    fixing bugs

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,12 @@
 /* eslint-disable no-undef */
-import dotenv from 'dotenv'
-import path from 'path'
+import dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config({ path: path.join(process.cwd(), '.env') })
+dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
   port: process.env.PORT,
   env: process.env.NODE_ENV,
   database_url: process.env.DB,
   default_user_password: process.env.DEFAULT_USER_PASSWORD,
-}
+};
